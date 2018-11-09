2e2cd0fc009a70474b15a39c0de7da729ece2af1
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-09 18:05:44 +0100

    disable logins for tests

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,7 +1,12 @@
-from twisted.trial.unittest import TestCase
-from test_server_utils import TestServer
+import logging
+
 from mock import patch
+from twisted.trial.unittest import TestCase
+
 from aduser.plugins import example_maxmind_geoip, example_browscap
+from test_server_utils import TestServer
+
+logging.disable(logging.WARNING)
 
 
 class MaxmindTestServer(TestServer):
