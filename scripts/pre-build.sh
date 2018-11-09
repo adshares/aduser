b7ccb4957cb1c5bb1b17e8514de0e59d6053faff
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-09 17:55:33 +0100

    make build and install conditional

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -9,21 +9,27 @@ apt-get -qq -y install python python-pip
 
 pip install pipenv
 
-## Install pybrowscap
-echo "Installing pybrowscap"
-CWD=`pwd`
-TEMP_DIR=`mktemp -d`
+if [ -v ${BUILD_WITH_PYBROWSCAP} ]; then
 
-cd $TEMP_DIR
-python -c "from urllib import urlretrieve; \
-           urlretrieve('https://github.com/char0n/pybrowscap/archive/master.zip', \
-           '$TEMP_DIR/pybrowscap.zip')"
+    ## Install pybrowscap
+    echo "Installing pybrowscap"
+    CWD=`pwd`
+    TEMP_DIR=`mktemp -d`
 
-python -m zipfile -e $TEMP_DIR/pybrowscap.zip $TEMP_DIR/
-mv -n $TEMP_DIR/pybrowscap-master/pybrowscap $BUILD_PATH/
+    cd $TEMP_DIR
+    python -c "from urllib import urlretrieve; \
+               urlretrieve('https://github.com/char0n/pybrowscap/archive/master.zip', \
+               '$TEMP_DIR/pybrowscap.zip')"
 
-cd $CWD
-rm -r $TEMP_DIR
-echo "Pybrowscap installed to: $BUILD_PATH/pybrowscap"
+    python -m zipfile -e $TEMP_DIR/pybrowscap.zip $TEMP_DIR/
+    mv -n $TEMP_DIR/pybrowscap-master/pybrowscap $BUILD_PATH/
 
-pipenv run install_geoip
+    cd $CWD
+    rm -r $TEMP_DIR
+    echo "Pybrowscap installed to: $BUILD_PATH/pybrowscap"
+
+fi
+
+if [ -v ${BUILD_WITH_GEOIP} ]; then
+    pipenv run install_geoip
+fi
--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -9,32 +9,40 @@ apt-get -qq -y install python python-pip
 
 pip install pipenv
 
-CWD=`pwd`
-mkdir -p ${ADUSER_DATA_PATH}
-TEMP_DIR=`mktemp -d`
+if [ -v ${INSTALL_GEOLITE_DATA} ]; then
 
-cd $TEMP_DIR
-python -c "from urllib import urlretrieve; \
-           urlretrieve('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz', \
-           '$TEMP_DIR/GeoLite2-City.tar.gz'); \
-           \
-           import tarfile; \
-           tar = tarfile.open('$TEMP_DIR/GeoLite2-City.tar.gz', 'r:gz'); \
-           version = sorted(tar.getnames())[0]; \
-           mmdb_file = tar.getmember(version + '/GeoLite2-City.mmdb'); \
-           tar.extract(mmdb_file); \
-           tar.close(); \
-           \
-           import shutil; \
-           shutil.move(version + '/GeoLite2-City.mmdb', '$ADUSER_DATA_PATH/GeoLite2-City.mmdb')"
+    echo "Downloading geolite data"
+    CWD=`pwd`
+    mkdir -p ${ADUSER_DATA_PATH}
+    TEMP_DIR=`mktemp -d`
+    cd ${TEMP_DIR}
+    python -c "from urllib import urlretrieve; \
+               urlretrieve('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz', \
+               '${TEMP_DIR}/GeoLite2-City.tar.gz'); \
+               \
+               import tarfile; \
+               tar = tarfile.open('${TEMP_DIR}/GeoLite2-City.tar.gz', 'r:gz'); \
+               version = sorted(tar.getnames())[0]; \
+               mmdb_file = tar.getmember(version + '/GeoLite2-City.mmdb'); \
+               tar.extract(mmdb_file); \
+               tar.close(); \
+               \
+               import shutil; \
+               shutil.move(version + '/GeoLite2-City.mmdb', '${ADUSER_DATA_PATH}/GeoLite2-City.mmdb')"
 
-cd  $CWD
-rm -r $TEMP_DIR
+    cd  ${CWD}
+    rm -r ${TEMP_DIR}
+    echo "Download finished"
 
-mkdir -p ${ADUSER_DATA_PATH}
-#python -c "import urllib; \
-#           class AdUserInstallerURLopener(urllib.FancyURLopener): ; \
-#               version = 'AdUser installer/1.0'; \
-#           urllib._urlopener = AdUserInstaller(); \
-#           urllib.urlretrieve('https://browscap.org/stream?q=BrowsCapCSV', \
-#           '$ADUSER_DATA_PATH/browscap.csv')"
+fi
+
+if [ -v ${INSTALL_BROWSCAP_DATA} ]; then
+
+    echo "Download brwoscap data"
+    mkdir -p ${ADUSER_DATA_PATH}
+    python -c "from pybrowscap.loader import Downloader; \
+               from pybrowscap.loader.csv import URL; \
+               Downloader(URL).get('${ADUSER_DATA_PATH}/browscap.csv')"
+    echo "Download finished"
+
+fi
