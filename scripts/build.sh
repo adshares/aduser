b720f36afc44c1bbb3161ee63f83c7533a9ab51c
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-12 13:13:27 +0100

    change env var name

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ if [ ! -v TRAVIS ]; then
   git clone \
     --depth=1 \
     https://github.com/adshares/aduser.git \
-    --branch ${ADUSER_INSTALLATION_BRANCH} \
+    --branch ${BUILD_BRANCH} \
     ${BUILD_PATH}/build
 
   cd ${BUILD_PATH}/build
