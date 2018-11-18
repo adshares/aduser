b44c772698426654d54215d00d68a6af2c19fdc0
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-18 20:42:27 +0100

    use print function

--- aduser/plugins/simple/tools/generate_schema_browscap.py
+++ aduser/plugins/simple/tools/generate_schema_browscap.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import csv
 import os
 from collections import defaultdict
@@ -22,25 +24,26 @@ with open(os.path.join(os.getenv('ADUSER_DATA_PATH'), 'browscap.csv'), 'rb') as 
                 sets[key].add(row[index[key]])
 
 for key in sorted(sets):
-    print key
+
+    print(key)
     if key == 'Browser':
         values_list = []
         for l in sorted(sets[key]):
             values_list.append({"key": l.lower().replace(' ', '_'),
-                   "label": l})
-        print values_list
+                                "label": l})
+        print(values_list)
     if key.startswith('Device_Type'):
         values_list = []
         for l in sorted(sets[key]):
             values_list.append({"key": l.lower().replace(' ', '_'),
-                   "label": l})
-        print values_list
+                                "label": l})
+        print(values_list)
     if key == 'Platform':
         values_list = []
         for l in sorted(sets[key]):
             values_list.append({"key": l.lower().replace(' ', '_'),
-                   "label": l})
-        print values_list
+                                "label": l})
+        print(values_list)
 
     if key.startswith('JavaScript'):
-        print sorted(sets[key])
+        print(sorted(sets[key]))
