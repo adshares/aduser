bc90f029d4c7d2e821712595e2add6249472a852
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-26 21:27:37 +0100

    update pipfile with sphinx and custom pybrowscap

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -10,24 +10,7 @@ apt-get -qq -y install --no-install-recommends python python-pip python-dev gcc
 pip install pipenv
 
 if [ -v BUILD_WITH_PYBROWSCAP ]; then
-
-    ## Install pybrowscap
-    echo "Installing pybrowscap"
-    CWD=`pwd`
-    TEMP_DIR=`mktemp -d`
-
-    cd $TEMP_DIR
-    python -c "from urllib import urlretrieve; \
-               urlretrieve('https://github.com/char0n/pybrowscap/archive/master.zip', \
-               '$TEMP_DIR/pybrowscap.zip')"
-
-    python -m zipfile -e $TEMP_DIR/pybrowscap.zip $TEMP_DIR/
-    mv -n $TEMP_DIR/pybrowscap-master/pybrowscap $BUILD_PATH/
-
-    cd $CWD
-    rm -r $TEMP_DIR
-    echo "Pybrowscap installed to: $BUILD_PATH/pybrowscap"
-
+    pipenv run install_pybrowscap
 fi
 
 if [ -v BUILD_WITH_GEOIP ]; then
