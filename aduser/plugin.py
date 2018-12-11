2fbeef0de4803fd8c4ad5c96a4db793b2858c887
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-12-11 14:01:28 +0100

    simple plugin system

--- aduser/plugin.py
+++ aduser/plugin.py
@@ -1,32 +1,22 @@
 from __future__ import print_function
 
-import glob
-import imp
-import os
-from os.path import basename, dirname
+import importlib
+
+from aduser import const
 
 #: Attribute where the data plugin is instantiated.
 data = None
 
 
-def initialize(name):
+def initialize():
     """
     Initialize the plugin by name.
     Searches for a package with the same name in ADUSER_PLUGINS_PATH and imports it.
     Allows access via `data` attribute.
 
-    :param name: Name of the data plugin.
     :return:
     """
     global data
 
-    plugin_files = glob.glob(os.path.join(os.getenv('ADUSER_PLUGINS_PATH'), "*"))
-    for p in plugin_files:
-        if os.path.isdir(p) and basename(p) == name:
-            possible_module_info = imp.find_module(basename(p), [dirname(p)])
-            possible_plugin = imp.load_module('aduser.plugins.' + name, *possible_module_info)
-
-            data = possible_plugin
-            data.init()
-
-            break
+    data = importlib.import_module(const.ADUSER_DATA_PROVIDER)
+    data.init()
