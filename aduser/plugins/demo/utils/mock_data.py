0f05196200f8d549df4fea9b2a2ff53296e2365a
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-12-13 16:50:33 +0100

    add new demo plugin

---
+++ aduser/plugins/demo/utils/mock_data.py
@@ -0,0 +1,6 @@
+default_mock = {"label": "Interest",
+        "key": "interest",
+        "type": "dict",
+                "data": [{"key": "1", "label": "Music"},
+                         {"key": "2", "label": "Books"},
+                         ]}
