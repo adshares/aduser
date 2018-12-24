f026a3133ce1dad97f4bd3e455f90fbf70db80f1
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-12-24 17:10:47 +0100

    documentation update

--- aduser/const.py
+++ aduser/const.py
@@ -2,27 +2,38 @@ import os
 import re
 from datetime import timedelta
 
-#: Twisted TCP port number
+#: Twisted TCP port number, ie. AdUser server port
+#:
+#: `Environmental variable override: ADUSER_PORT`
 SERVER_PORT = int(os.getenv('ADUSER_PORT', 9090))
 
-#: Secret used for creating a tracking id
+#: Secret used for creating a tracking id.
+#:
+#: `Environmental variable override: ADUSER_TRACKING_SECRET`
 SECRET = os.getenv('ADUSER_TRACKING_SECRET', 'ChangeMe!')
 
-#: Secret used for creating a tracking id
+#: Configurable path to pixel. Becomes start of pixel request paths returned by getPixelPath.
+#:
+#: `Environmental variable override: ADUSER_PIXEL_PATH`
 PIXEL_PATH = os.getenv('ADUSER_PIXEL_PATH', 'path_to_pixel')
 
-#: Cookie key
-COOKIE_NAME = bytes(os.getenv('ADUSER_COOKIE_NAME', 'ChangeMe'))
+#: Name of the cookie used for tracking.
+#:
+#: `Environmental variable override: ADUSER_COOKIE_NAME`
+COOKIE_NAME = bytes(os.getenv('ADUSER_COOKIE_NAME', 'AdsharesAdUserTracking'))
 
-#: Name of AdUser data backend plugin
+#: Name of AdUser data backend plugin.
+#:
+#: `Environmental variable override: ADUSER_DATA_PROVIDER`
 DATA_PROVIDER = os.getenv('ADUSER_DATA_PROVIDER', 'aduser.plugins.examples.example')
 
-#: Expiry period, accepts 'w' for weeks and 'd' for days. Format: {num}{format}, eg. 4w for 4 weeks.
+#: Tracking cookie expiry period. The enviromental variable accepts 'w' for weeks and 'd' for days. Format: {num}{format}, eg. '`4w`' for 4 weeks.
+#:
+#: `Environmental variable override: ADUSER_EXPIRY_PERIOD`
+EXPIRY_PERIOD = timedelta(weeks=4)
 config_period = re.match('^(\d+)(\w)$', os.getenv('ADUSER_EXPIRY_PERIOD', '4w'))
 
-if not config_period:
-    EXPIRY_PERIOD = timedelta(weeks=4)
-else:
+if config_period:
     count = int(config_period.group(1))
 
     if config_period.group(2) == 'w':
