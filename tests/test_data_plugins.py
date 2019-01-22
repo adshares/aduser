059983d7f2c5ae98284c4d74882abef01653b114
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2019-01-22 15:51:47 +0100

    docs & tests

--- tests/test_data_plugins.py
+++ tests/test_data_plugins.py
@@ -1,37 +1,65 @@
-from mock import mock_open, patch
+from mock import MagicMock, mock_open, patch
 from twisted.internet import defer
 from twisted.trial.unittest import TestCase
 
-from aduser.data import demo
+import aduser
+from aduser.data import configure_plugin, demo
 from aduser.data.demo import mock_data as demo_mock_data
 from test_server_utils import TestServer
 
 
-class ExampleBrowscapTestServer(TestServer):
+class PluginTestCase(TestCase):
+    data_plugin = 'aduser.data.examples.example'
+
+    def setUp(self):
+        with patch('aduser.data.const.DATA_PROVIDER', self.data_plugin):
+            configure_plugin()
+
+    def tearDown(self):
+        aduser.data.provider = None
+
+    def test_pixel(self):
+        req = MagicMock()
+
+        pixel = aduser.data.provider.pixel(req)
+        self.assertIsNotNone(pixel)
+        req.setHeader.assert_called_with(b"content-type", b"image/gif")
+
+    def test_update_data(self):
+        user_data = {'keywords': {}}
+        request_data = {'device': {"ua": "",
+                                   "ip": ""}}
+
+        data = aduser.data.provider.update_data(user_data, request_data)
+        self.assertIsNotNone(data)
+
+
+class ExampleBrowscapTestServer(PluginTestCase):
     data_plugin = 'aduser.data.examples.browscap'
 
 
-class ExampleExampleTestServer(TestServer):
+class ExampleExampleTestServer(PluginTestCase):
     data_plugin = 'aduser.data.examples.example'
 
 
 class ExampleIPapiTestServer(TestServer):
+    # This uses TestServer (reactor)
     data_plugin = 'aduser.data.examples.ipapi'
 
 
-class ExampleMaxmindTestServer(TestServer):
+class ExampleMaxmindTestServer(PluginTestCase):
     data_plugin = 'aduser.data.examples.maxmind_geoip'
 
 
-class SkeletonTestServer(TestServer):
+class SkeletonTestServer(PluginTestCase):
     data_plugin = 'aduser.data.skeleton'
 
 
-class SimpleTestServer(TestServer):
+class SimpleTestServer(PluginTestCase):
     data_plugin = 'aduser.data.simple'
 
 
-class DemoTestServer(SimpleTestServer):
+class DemoTestServer(PluginTestCase):
     data_plugin = 'aduser.data.demo'
 
 
