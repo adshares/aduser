48c9fd460673eb7d897c9cee9ca758dccab29d63
Paweł Zaremba <pawzar@gmail.com>
2019-03-18 15:44:45 +0100

    z

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,7 +3,6 @@ set -e
 
 source ${1:-$(dirname $(readlink -f "$0"))/bin}/_functions.sh
 [[ -z ${2:-""} ]] || cd $2
-[[ -z ${3:-".env"} ]] || set -a && source .env && set +a
 
 composer install
-bin/console aduser:update
+#bin/console aduser:update #todo: +cron
