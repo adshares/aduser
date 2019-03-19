d9a98c1f3ce331174c5c7524dc9d23d20a54abdb
Paweł Zaremba <pawzar@gmail.com>
2019-03-19 14:38:01 +0100

    dry_run

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,5 +2,7 @@
 source ${1}/_functions.sh --vendor
 [[ -z ${2:-""} ]] || cd $2
 
+[[ ${DRY_RUN:-0} -eq 1 ]] && echo "DRY-RUN: $0 $@" && exit 127
+
 composer install
-#bin/console aduser:update #todo: +cron
+bin/console aduser:update #todo: +cron
