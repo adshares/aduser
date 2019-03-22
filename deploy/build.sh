fa2906b36b7b93da8ab91531bdee43619eb1c651
Paweł Zaremba <pawzar@gmail.com>
2019-03-22 15:34:09 +0100

    Migrations

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-source ${1}/_functions.sh --vendor
-[[ -z ${2:-""} ]] || cd $2
+if [[ -z ${1:-""} ]]
+then
+    set -eu
+else
+    source ${1}/_functions.sh --vendor
+fi
+cd ${2:-"."}
 
 export APP_VERSION=$(versionFromGit)
 
 composer install
-bin/console aduser:update
 bin/console doctrine:migrations:migrate
+bin/console aduser:update
