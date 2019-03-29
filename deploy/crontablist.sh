b1fe1581dd39b5dc8f272ca5854dd7a4dc1f3e8e
Paweł Zaremba <pawzar@gmail.com>
2019-03-29 13:43:42 +0100

    Better crontab

--- deploy/crontablist.sh
+++ deploy/crontablist.sh
@@ -2,5 +2,13 @@
 set -eu
 
 SERVICE_DIR=${SERVICE_DIR:-$(dirname $(dirname $(readlink -f $0)))}
+LOG_DIR=${LOG_DIR:-""}
 
-echo "0 0 * * * php ${SERVICE_DIR}/bin/console aduser:update &> /dev/null"
+if [[ -z ${LOG_DIR} ]]
+then
+    _REDIRECTION="&> /dev/null"
+else
+    _REDIRECTION="&>> ${LOG_DIR}/aduser-crontab.log"
+fi
+
+echo "0 0 * * * php ${SERVICE_DIR}/bin/console aduser:update ${_REDIRECTION}"
