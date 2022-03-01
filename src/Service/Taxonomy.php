f06ad37b749cf438a29a85b39eba74597031d798
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2022-03-01 09:52:22 +0100

    Fix the Apple OS key

--- src/Service/Taxonomy.php
+++ src/Service/Taxonomy.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright (c) 2018-2022 Adshares sp. z o.o.
+ *
+ * This file is part of AdUser
+ *
+ * AdUser is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdUser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdServer. If not, see <https://www.gnu.org/licenses/>
+ */
 
 declare(strict_types=1);
 
@@ -501,7 +519,7 @@ final class Taxonomy
             case 'iOS':
             case 'macOS':
             case 'MacOSX':
-                return 'apple_os';
+                return 'apple-os';
             case 'AIX':
             case 'Linux':
             case 'BSD':
